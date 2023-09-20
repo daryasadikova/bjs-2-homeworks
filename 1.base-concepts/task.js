129090c3d69886d087a4c41e100e41b2007c1238
Daria Sadikova <da.sadikova94@gmail.com>
2023-09-20 11:31:23 +0300

    корректировка

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,24 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
+  let discr = (b**2) - (4 * a * c);
+  let root = Math.sqrt(discr);
+  if (discr > 0) {
+    let firstResult = (-b + root) / (2 * a);
+    let secondResult = (-b - root) / (2 * a);
+    arr.push(firstResult, secondResult);
+  } else if (discr === 0) {
+    arr.push(-b / (2 * a));
+  } 
+    return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+    let percentNew = (percent / 100) / 12;
+    let body = amount - contribution;
+    let monthPay = body * (percentNew + (percentNew / (((1 + percentNew) ** countMonths) - 1)));
+    let allAmount = monthPay * countMonths;
+    let allAmountRound = allAmount.toFixed(2);
+    let result = parseFloat(allAmountRound);
+    return result;
 }
